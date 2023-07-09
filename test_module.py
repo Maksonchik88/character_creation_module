37f9907d5e7bab0d1c501c73255758e9b1004753
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-07-09 17:41:39 +0300

    code according to PEP8 and annotations

--- test_module.py
+++ test_module.py
@@ -7,7 +7,7 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 files_list = ['main.py', 'readme.md']
 
 
-def test_program():
+def test_program() -> None:
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+        assert filename in dir_files, (f'Файл `{filename}` не найден'
+                                       ' в корне репозитория')
